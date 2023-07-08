aa821990776c03ca6d6d901a909b7bd3f0caa07c
Linh Lien <lienx149@gmail.com>
2023-07-08 14:06:40 -0500

    added comments

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,8 @@
 let body = document.querySelector('#intro');
 let hEl = document.createElement('h1');
 let headerEl = document.createElement('header');
-let score = 0;
-let secondsLeft = 60;
-let i = 0
 
+// these are elemts that goes with buttons
 let divEl = document.createElement('div');
 let divElA = document.createElement('div');
 let divElB = document.createElement('div');
@@ -13,20 +11,28 @@ let divElD = document.createElement('div');
 let divElE = document.createElement('div');
 let timeDiv = document.createElement('div');
 let ViewScore = document.createElement('div');
+
 // sections to each page
 let sectionEl = document.createElement('section');
 let sectionEl2 = document.createElement('section');
 let sectionEl3 = document.createElement('section');
+let sectionEl4 = document.createElement('section');
 
 let inputEl = document.createElement('input');
+
+// these are button elements
 let button = document.createElement('button');
 let clear = document.createElement('button');
 let goBack = document.createElement('button');
 
-
+// these are the initials and scores array
 let scoreArray = [];
 let initial = [];
 
+let score = 0;
+let secondsLeft = 60;
+let i = 0
+// questions for quiz
 let questions = [
     {
         question: 'What is the name of the village Luffy was born in??',
@@ -61,21 +67,18 @@ let questions = [
         answer: 'To defeat Hawk Eye and become the greatest swordsman.',
     }
 ]
-let sectionEl4 = document.createElement('section');
-
 
 
 sectionEl4.remove();
-
+// this is where we appendchild to elements
 body.appendChild(headerEl);
 headerEl.appendChild(ViewScore).textContent = "VIEW SCORE"
 headerEl.appendChild(timeDiv).textContent = ' '
-
 body.appendChild(sectionEl);
 sectionEl.appendChild(hEl).textContent = "Ready to Play?";
 sectionEl.appendChild(divEl).textContent = 'press to play';
 
-    //Click handler (Start Page button)
+//Click handler (Start Page button)
     divEl.addEventListener('click', function () {
         sectionEl.remove();
         ViewScore.remove();
@@ -89,17 +92,12 @@ sectionEl.appendChild(divEl).textContent = 'press to play';
                 clearInterval(timerInterval);
                 headerEl.appendChild(timeDiv);
                 timeDiv.textContent = 'Game Over';
-
                 console.log(score);
                 return gameOver();
             }
-
-        }, 500);
-
+        }, 1000);
         return questionPopulator(i);
-
     });
-
 //Click handler (SectionEl2)
 sectionEl2.addEventListener('click', function (event) {
     let element = event.target;
@@ -110,20 +108,18 @@ sectionEl2.addEventListener('click', function (event) {
     } else if (element.getAttribute('id') == "inc") {
         i++;
         secondsLeft = secondsLeft - 20;
-
         return questionPopulator(i);
     }
 });
 
-
+//Appending question to sectionEl2
+//Appending answer choices to sectionEl2
+//setting ids based on correct or incorrect answer
     function questionPopulator(i) {
     if (i == questions.length || secondsLeft < 1) {
         return gameOver();
     } else {
-
-        //Appending question to sectionEl2
-        //Appending answer choices to sectionEl2
-        //setting ids based on correct or incorrect answer
+        
         sectionEl2.appendChild(hEl).textContent = questions[i].question;
         sectionEl2.appendChild(divElA).textContent = questions[i].A;
         if (questions[i].A == questions[i].answer) {
@@ -143,24 +139,19 @@ sectionEl2.addEventListener('click', function (event) {
         } else divElD.setAttribute("id", "inc")
     }
 }
-function gameOver() {
-    //if we finished the last question,
-    //Wipe away sectionEl2
-    //Populate sectionEl3, the finish page
 
+//This is the last page of the game where one will enter initials and see score
+function gameOver() {
     sectionEl2.remove();
     sectionEl3.remove();
     sectionEl4.remove();
-
     body.appendChild(sectionEl3);
     sectionEl3.appendChild(hEl).textContent = 'Congrats! Your score is: ' + score;
-
-
     sectionEl3.appendChild(button).textContent = 'Enter initials here ';
     sectionEl3.appendChild(inputEl);
-
 }
 
+// this renders the score and clears it or go back to main page
 function renderScore() {
     sectionEl3.remove();
     body.appendChild(sectionEl4);
@@ -175,9 +166,8 @@ function renderScore() {
         li.textContent = 'INITIAL: ' + init + ' | SCORE: ' + sc;
         sectionEl4.appendChild(li);
     }
-
 }
-
+// this is where you store information
 function init() {
     let storedInitals = JSON.parse(localStorage.getItem('initials'));
     let storedScores = JSON.parse(localStorage.getItem('scores'));
@@ -187,8 +177,8 @@ function init() {
     if (storedInitals !== null) {
         initial = storedInitals;
     }
-    
 }
+
 function storeScores() {
     localStorage.setItem('initials', JSON.stringify(initial));
     localStorage.setItem('scores', JSON.stringify(scoreArray));
@@ -196,7 +186,6 @@ function storeScores() {
 
 //this is what happens when we click the button
 button.addEventListener('click', function () {
-
     let initials = inputEl.value.trim();
     if (initials === '') {
         return;
@@ -207,23 +196,9 @@ button.addEventListener('click', function () {
     // code to render here
     storeScores();
     renderScore();
-
 });
-//this is what happens when we click the button
-button.addEventListener('click', function () {
 
-    let initials = inputEl.value.trim();
-    if (initials === '') {
-        return;
-    }
-    initial.push(initials);
-    scoreArray.push(score);
-    inputEl.value = '';
-    // code to render here
-    storeScores();
-    renderScore();
-
-});
+// this clears the score
 clear.addEventListener('click', function () {
     sectionEl4.textContent = '';
     localStorage.clear();
@@ -231,6 +206,8 @@ clear.addEventListener('click', function () {
     initial = [];
     renderScore();
 });
+
+// this goes back to the start page
 goBack.addEventListener('click', function () {
     goBack.remove();
     score = 0;
@@ -239,18 +216,14 @@ goBack.addEventListener('click', function () {
     headerEl.textContent = '';
     headerEl.appendChild(ViewScore);
     sectionEl4.remove();
-
     body.appendChild(sectionEl);
     sectionEl.appendChild(hEl).textContent = "Ready to Play?";
     sectionEl.appendChild(divEl).textContent = 'press to play';
-
-
 });
+
+// this is the button to view the score
 ViewScore.addEventListener('click', function () {
     sectionEl.remove();
-
     renderScore();
 })
-
-
 init();
